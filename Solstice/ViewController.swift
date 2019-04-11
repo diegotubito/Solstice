5b6ea34f575c13aa12a89cb1ed6f828b04ce384c
iMac <faflores@hipotecario.com.ar>
2019-04-11 11:37:49 -0300

    Initial Commit

---
+++ Solstice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solstice
+//
+//  Created by iMac on 11/04/2019.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
